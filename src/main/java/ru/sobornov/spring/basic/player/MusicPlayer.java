3ab9671f8cf92bc656c25e0f8ed91d593bb8a3a5
Vladimir Sobornov <thecodegod1@gmail.com>
2022-10-05 01:52:42 +0300

    Inversion of Control Example Injecting Music objects into MusicPlayer Added dependency injection via XML + setter

--- src/main/java/ru/sobornov/spring/basic/player/MusicPlayer.java
+++ src/main/java/ru/sobornov/spring/basic/player/MusicPlayer.java
@@ -8,13 +8,18 @@ import ru.sobornov.spring.basic.music.Music;
  **/
 public class MusicPlayer {
     private Music music;
+    private int volume;
 
     public MusicPlayer(Music music) {
         this.music = music; //IoC
     }
 
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     public void getMusic() {
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + music.getSong() + "\n" + "Volume: " + volume);
     }
 
 
